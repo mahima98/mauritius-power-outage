0c7d8b2ae361c1220697765c8bd5e0fc02824cab
mahima98 <mahimaramgolam98@gmail.com>
2022-10-05 16:25:27 +0400

    Added power-outrage real countrylist data

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,44 +4,46 @@ import { HiOutlineLightBulb } from "react-icons/hi";
 
 export default function Header({ darkMode, setDarkMode }: any) {
   return (
-    <nav className="py-10 mb-12 md:mb-6 flex justify-between px-2 max-w-7xl  mx-auto">
-      <Link href="/">
-        <h1 className="cursor-pointer text-xl font-semibold font-burtons">
-          developedByMahi
-        </h1>
-      </Link>
-      <ul className="flex items-center gap-8">
-        <li className="px-4 py-2 rounded hover:bg-gray-100">
-          <Link href="/powerOutrage">
-            <a className="font-semibold">Power outrage</a>
-          </Link>
-        </li>
-        {!darkMode && (
-          <li>
-            <BsFillMoonStarsFill
-              onClick={() => setDarkMode(!darkMode)}
-              className="cursor-pointer text-2xl"
-            />
+    <nav className=" dark:bg-gray-600 ">
+      <div className="py-6 px-4 flex justify-between max-w-7xl mx-auto dark:border-b border-gray-500">
+        <Link href="/">
+          <h1 className="flex justify-center items-center rsor-pointer text-xl font-semibold font-burtons">
+            developedByMahi
+          </h1>
+        </Link>
+        <ul className="flex justify-center items-center gap-8">
+          <li className="md:px-4 px-2 py-2 rounded hover:bg-gray-100">
+            <Link href="/powerOutrage">
+              <a className="font-semibold text-sm">Power outrage</a>
+            </Link>
           </li>
-        )}
-        {darkMode && (
-          <li className="dark:text-white">
-            <HiOutlineLightBulb
-              onClick={() => setDarkMode(!darkMode)}
-              className="cursor-pointer text-3xl"
-            />
-          </li>
-        )}
+          {!darkMode && (
+            <li>
+              <BsFillMoonStarsFill
+                onClick={() => setDarkMode(!darkMode)}
+                className="cursor-pointer text-2xl"
+              />
+            </li>
+          )}
+          {darkMode && (
+            <li className="dark:text-white">
+              <HiOutlineLightBulb
+                onClick={() => setDarkMode(!darkMode)}
+                className="cursor-pointer text-lg md:text-3xl"
+              />
+            </li>
+          )}
 
-        <li>
-          <a
-            href="#"
-            className="bg-gradient-to-r from-cyan-500 to-teal-200 text-white dark:text-gray-800 px-4 py-2 rounded"
-          >
-            Resume
-          </a>
-        </li>
-      </ul>
+          <li>
+            <a
+              href="#"
+              className="bg-gradient-to-r from-cyan-500 to-teal-200 text-white dark:text-gray-800 px-4 py-2 rounded"
+            >
+              Resume
+            </a>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
--- pages/powerOutrage.tsx
+++ pages/powerOutrage.tsx
@@ -1,5 +1,94 @@
+import { Key, useEffect, useState } from "react";
+
+export interface product {
+  id: string;
+  brand: string;
+  category: string;
+}
+
+export interface Countries {
+  id: string;
+  date?: string;
+  district: string;
+  from: string;
+  to: string;
+}
+
+const API_ENDPOINT =
+  "https://raw.githubusercontent.com/MrSunshyne/mauritius-dataset-electricity/main/data/power-outages.json";
+// const API_ENDPOINT = "https://dummyjson.com/products/category/smartphones";
+
 function powerOutrage() {
-  return <div></div>;
+  const [loadedOutrages, setLoadedOutrages] = useState([]);
+
+  useEffect(() => {
+    fetch(API_ENDPOINT)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => setLoadedOutrages(data));
+  }, []);
+
+  const countryList = loadedOutrages;
+
+  // key : "Values"
+  // Get array values
+  const items = Object.values(countryList).map((value) => {
+    console.log(countryList[value]);
+    return value;
+  });
+
+  // Get array keys
+  const itemsKeys = Object.keys(countryList).map((key) => {
+    return key;
+  });
+
+  console.log(itemsKeys);
+
+  return (
+    <div className="power-outrage dark:bg-gray-600">
+      <div className="p-6 flex flex-col gap-4 max-w-7xl mx-auto ">
+        {Object.keys(countryList).map((key, i: Key) => (
+          <div
+            key={i}
+            className="p-4 bg-gray-100 rounded-lg inline-block flex flex-col gap-4 "
+          >
+            <div className="font-bold text-xl uppercase tracking-wide">
+              {key}
+            </div>
+            <div className="">
+              {items[i]
+                .slice(0)
+                .reverse()
+                .slice(0, 10)
+                .map(
+                  (
+                    value: { district: string; from: string; to: string },
+                    i: Key
+                  ) => (
+                    <div
+                      key={i}
+                      className="p-4 m-1 bg-gray-200 inline-block rounded-lg"
+                    >
+                      <div>{value.district}</div>
+                      <div>
+                        from ({" "}
+                        {new Intl.DateTimeFormat("en-GB", {
+                          month: "long",
+                          day: "2-digit",
+                          year: "numeric",
+                        }).format(new Date(value.from))}
+                        )
+                      </div>
+                    </div>
+                  )
+                )}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default powerOutrage;
