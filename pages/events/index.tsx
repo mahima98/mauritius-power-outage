64ae36d61638da74fb959b63ba26f6aa422ef289
mahima98 <mahimaramgolam98@gmail.com>
2022-10-21 16:43:08 +0400

    event page dark mode added

--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -1,6 +1,6 @@
 export default function events({ events }) {
   return (
-    <div className="px-4 pt-16 pb-20 bg-white sm:px-6 lg:px-8 lg:pt-24 lg:pb-28">
+    <div className="px-4 pt-16 pb-20 bg-white sm:px-6 lg:px-8 lg:pt-24 lg:pb-28 dark:bg-gray-600">
       <div className="relative max-w-lg mx-auto divide-y-2 divide-gray-200 lg:max-w-7xl">
         <div>
           <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
