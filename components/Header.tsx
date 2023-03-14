1bb1c1762cd5155c43ccd2d845d1aab70410c7cd
mahima98 <mahimaramgolam98@gmail.com>
2023-03-14 11:26:40 +0400

    fix header

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,11 +6,11 @@ export default function Header({ darkMode, setDarkMode }: any) {
   const routes = [
     {
       name: "Power outage",
-      path: "/powerOutage",
+      path: "/powerOutrage",
     },
     {
       name: "Power outage",
-      path: "/powerOutage",
+      path: "/powerOutrage",
     },
     {
       name: "Events",
@@ -45,12 +45,12 @@ export default function Header({ darkMode, setDarkMode }: any) {
         </Link>
         <ul className="flex items-center justify-center md:gap-2">
           <li className="px-2 py-2 rounded md:px-2 hover:bg-gray-100">
-            <Link href="/powerOutage">
+            <Link href="/powerOutrage">
               <a className="text-xs font-semibold md:text-sm">Power outage</a>
             </Link>
           </li>
           <li className="px-2 py-2 rounded md:px-2 hover:bg-gray-100">
-            <Link href="/powerOutageSsr">
+            <Link href="/powerOutrageSsr">
               <a className="text-xs font-semibold md:text-sm">
                 Power outage(SSR)
               </a>
